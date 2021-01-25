54b66d95f93bc35b33f076a42f0122a409a73920
SpaceChemist <cleverenz21@gmail.com>
2021-01-25 00:07:28 -0800

    fuuucckkkkkkkk xaml

--- Referrals project/ReferralConfig.cs
+++ Referrals project/ReferralConfig.cs
@@ -4,6 +4,33 @@ namespace Referrals_project
 {
     public class ReferralConfig : ViewModel
     {
+        private bool _referralrewards = true;
+        private bool _promotionrewardenabled = true;
+        private string _serverreferralcode = "serverntest code";
+        private string _promotionrewardscode = "Promotiontestcode";
         
+        public bool ReferralRewardsEnabled
+        {
+            get => _referralrewards;
+            set => SetValue(ref _referralrewards, value);
+        }
+        
+        public bool PromotionRewardEnabled
+        {
+            get => _promotionrewardenabled;
+            set => SetValue(ref _promotionrewardenabled, value);
+        }
+        
+        public string ServerReferralCode
+        {
+            get => _serverreferralcode; 
+            set => SetValue(ref _serverreferralcode, value);
+        }
+        
+        public string PromotionRewardsCode
+        {
+            get => _promotionrewardscode;
+            set => SetValue(ref _promotionrewardscode, value);
+        }
     }
 }
